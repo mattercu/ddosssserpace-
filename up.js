e73bfb02421cec30d261a8d87571325d1f16deea
HanhOTP <hoanganhphone2009@gmail.com>
2025-10-28 23:06:19 +0700

    Fix GitHub URLs in up.js

---
+++ up.js
@@ -0,0 +1,7 @@
+// SAI:
+this.githubVersionUrl = "https://raw.githubusercontent.com/mattercu/hanh/refs/heads/main/version.json";
+this.githubScriptUrl = "https://raw.githubusercontent.com/mattercu/hanh/refs/heads/main/hanh.js";
+
+// ĐÚNG:
+this.githubVersionUrl = "https://raw.githubusercontent.com/mattercu/hanh/main/version.json";
+this.githubScriptUrl = "https://raw.githubusercontent.com/mattercu/hanh/main/hanh.js";
